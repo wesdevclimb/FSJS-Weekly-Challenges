9f9a232db5cf523334569dc5b268e2ddcf12b946
wesdevclimb <wesleyjohnson.stu@gmail.com>
2016-09-07 13:03:59 -0400

    Solved week 2 challenge in the js file

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -1,8 +1,16 @@
 //create function
+function evaluate(message){
   //control and return statements
+  if (message === 'hello') {
+    alert('Hello, world!');
+  }
+  else if (message !== 'hello') {
+    alert("Sorry. You didn't enter the desired response.");
+  }
+}
 
 //Prompt user for input and store variable
-
+var userResponse = prompt('Enter the word "Hello" to bring the program to life!');
 //Call function
-
+evaluate(userResponse.toLowerCase());
 //Alert user results
