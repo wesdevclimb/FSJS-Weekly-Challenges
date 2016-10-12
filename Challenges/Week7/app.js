60b0ac5d0a58d94a94df1472c88aeaa4d04bfd3c
wesdevclimb <wesleyjohnson.stu@gmail.com>
2016-10-11 23:05:22 -0400

    Completed Task 1 and Task 2 of the week 7 challenge

--- Challenges/Week7/app.js
+++ Challenges/Week7/app.js
@@ -1,1 +1,6 @@
 //See readme for challenge instructions
+//Task 1: Require the github.js file for access to github.getRepos() in app.js
+var github = require("./github");
+
+
+github.getRepos('bluerman');
--- Challenges/Week7/github.js
+++ Challenges/Week7/github.js
@@ -2,7 +2,7 @@
 
 /*
  * Note about github api: requires User-Agent header to be set. This can be done
- * in Node by passing an options object (rather than a simple url string) as the 
+ * in Node by passing an options object (rather than a simple url string) as the
  * first param to the https.get() function
  *
  * So something like
@@ -16,6 +16,46 @@
  *
  */
 
-function getRepos(username){
-  console.log('Repos for ' + username);
+var https = require("https");
+
+function printMessage(username, array){
+  //The printMessage() needs to iterate over each item in the array and log out the name of each repository
+  var message = username + " has " + array.length + " repos. \n";
+  array.forEach(function(arrayItem){
+    message += "* " + arrayItem.name + "\n";
+  });
+  console.log(message);
 }
+
+
+
+function getRepos(username){
+  //Task 2: Configure options by creating a JavaScript object
+  var options = {
+    'hostname' : 'api.github.com',
+    'path' : '/users/' + username + '/repos',
+    //'headers' object must be set or a 403 error will be caused
+    'headers' : {
+      'User-Agent' : 'wesdevclimb'
+    }
+  };
+
+  //Create the https.get request
+  var request = https.get(options, function(response){
+  //Concatenate data stream into the body
+    var body = "";
+    response.on('data', function(chunk){
+      body += chunk;
+    });
+    response.on('end', function(){
+      var repos = JSON.parse(body);
+      printMessage(username, repos);
+    });
+  });
+  request.end();
+}
+
+//Should handle common errors
+
+//Task 1: Export getRepos() module
+module.exports.getRepos = getRepos;
